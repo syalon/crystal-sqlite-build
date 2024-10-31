5a4ded47282124a5e2d86ce92722aa7210fa395c
syalon <empty@email.com>
2024-10-31 17:17:18 +0800

    update compile script

--- auto_build_sqlite.sh
+++ auto_build_sqlite.sh
@@ -11,8 +11,9 @@ CONFIGURE_FILE="$SOURCE_CLONE_DIR/configure"
 # -f 参数判断配置文件是否存在 不存在才 clone，不然每次 clone 很慢。
 if [ ! -f "$CONFIGURE_FILE" ]; then
 	rm -rf $SOURCE_CLONE_DIR
-	git clone https://github.com/sqlite/sqlite.git $SOURCE_CLONE_DIR && cd $SOURCE_CLONE_DIR && git checkout $COMPILE_VERSION
+	git clone -b $COMPILE_VERSION https://github.com/sqlite/sqlite.git $SOURCE_CLONE_DIR --depth=1
 fi
+cd $SOURCE_CLONE_DIR && git checkout $COMPILE_VERSION
 
 # 2、编译 / compile
 ./configure
